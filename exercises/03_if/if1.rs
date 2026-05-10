40c345cf605624210ed03cbdce1eeaff456af110
Seunghwan Chun <zshchun@gmail.com>
2026-05-11 01:24:41 +0900

    Complete exercises through 03

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,13 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b { a } else { b }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let x = bigger(1, 3);
+    println!("result : {x}");
 }
 
 // Don't mind this for now :)
