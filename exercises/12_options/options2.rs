502e4cc7d944e21d75a9abd22bf61d6f981c4795
Seunghwan Chun <zshchun@gmail.com>
2026-05-19 23:54:25 +0900

    solved 12_options

--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -29,7 +29,7 @@ mod tests {
         // TODO: Make this a while-let statement. Remember that `Vec::pop()`
         // adds another layer of `Option`. You can do nested pattern matching
         // in if-let and while-let statements.
-        while let Some(integer) = optional_integers.pop() {
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
